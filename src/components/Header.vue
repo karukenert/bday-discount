9affdef5517a156ede1fd8e47b3427f6681cf4d0
karukenert <karukenert@gmail.com>
2025-01-18 15:18:16 +0200

    run linter

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 
- defineProps<{ msg: string }>()
+defineProps<{ msg: string }>()
 </script>
 
 <template>
-    <h1>{{msg}}</h1>
+  <h1>{{ msg }}</h1>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
-import { createApp } from 'vue'
+import Aura from '@primevue/themes/aura'
 
+import PrimeVue from 'primevue/config'
+import { createApp } from 'vue'
 import App from './App.vue'
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
 
 import './style.css'
 
-
-const app  = createApp(App)
-app.use(PrimeVue,  {
-    theme: {
-        preset: Aura
-    }
+const app = createApp(App)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
 })
 app.mount('#app')
