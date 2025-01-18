92cfc0fb751ecc47a8f2b2b9b4bfa583753cac7f
karukenert <karukenert@gmail.com>
2025-01-18 15:18:33 +0200

    add table component

---
+++ src/components/Table.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Column, DataTable } from 'primevue'
+import { data } from '../data/data'
+import Container from './Container.vue'
+import Header from './Header.vue'
+</script>
+
+<template>
+  <Container x="1" y="1">
+    <Header msg="🎉 Sünnipäeva soodustused 🎉" />
+    <DataTable :value="data">
+      <Column field="companyName" header="Name" />
+      <Column field="discountName" header="Soodustus" />
+    </DataTable>
+  </Container>
+</template>
