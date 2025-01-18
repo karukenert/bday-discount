b3898b4d58e145a95ea2bd7623c5ed11c12c037c
karukenert <karukenert@gmail.com>
2025-01-18 14:03:43 +0200

    add sass and primevue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import Button from "primevue/button"
 
-createApp(App).mount('#app')
+
+
+const app  = createApp(App)
+app.use(PrimeVue,  {
+    theme: {
+        preset: Aura
+    }
+})
+app.component('Button', Button)
+app.mount('#app')
