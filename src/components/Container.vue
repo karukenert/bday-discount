270ceb4f1ee4de14da8cc2ca86b6162506f08f9f
karukenert <karukenert@gmail.com>
2025-01-18 14:50:38 +0200

    create generic container component

---
+++ src/components/Container.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    const { x = '0', y = '0' } = defineProps<{
+        x?: string
+        y?: string
+    }>()
+</script>
+
+<template>
+    <div :style="{ padding: `${y}rem ${x}rem` }">
+        <slot />
+    </div>
+</template>
